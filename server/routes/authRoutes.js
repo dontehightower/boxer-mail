2073b286a2a929775499831753a402640a8856f6
dontehightower <dontedhightower@gmail.com>
2018-07-07 16:04:47 -0500

    add authentication routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -10,5 +10,20 @@ module.exports = app => {
   );
 
   // perform code exchange when user visits auth/google/callback
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surveys');
+    }
+  );
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
 };
