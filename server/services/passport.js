491b934aa5e1620fa300bafb3401459539cbde3a
dontehightower <dontedhightower@gmail.com>
2018-07-07 16:05:39 -0500

    refactor passport.js and add serialize/deserialize user functions

--- server/services/passport.js
+++ server/services/passport.js
@@ -7,25 +7,37 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+// serializeUser takes in the user object, which refers to the mongoose
+// model, and stores a reference of the _id property to be used later.
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// deserializeUser searches the User class for a user with a matching id,
+//
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       // the route users are sent to after granting permission to app
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
+    async (accessToken, refreshToken, profile, done) => {
       // is there a user with this profile.id in the users collection?
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // there is a record with the given profile ID
-          done(null, existingUser);
-        } else {
-          // if not create a User model instance & save it to the DB
-          new User({ googleId: profile.id }).save().then(user => done(null, user));
-        }
-      });
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
